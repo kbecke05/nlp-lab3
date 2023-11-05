1426a3cfabd4d0b1425d573e3cd1ad4d48a8686f
Kelly Becker <kbecke05@calpoly.edu>
2023-11-05 00:08:23 -0700

    preprocessing functions added

--- chatFOL.py
+++ chatFOL.py
@@ -1,11 +1,18 @@
 import argparse
 import spacy
+import nltk
 from spacy import displacy
+from nltk import load_parser
+from spacy.matcher import PhraseMatcher
+from nltk.stem import WordNetLemmatizer
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(dest = "file", nargs="?")
     args = parser.parse_args()
+
+    nlp = spacy.load("en_core_web_sm")
+    nltk.download('wordnet')
 
     if args.file: # if text file is provided as an argument
         try:
@@ -13,7 +20,7 @@ def main():
                 lines = file.readlines()
                 for line in lines:
                     original_line = line.strip()
-                    translated_line = translate_line(original_line)
+                    translated_line = translate_line(original_line, nlp)
                     print(f"Translated: {translated_line}\n")
         except FileNotFoundError:
             print(f"Error: File '{args.file}' not found.")
@@ -25,19 +32,48 @@ def main():
                 break
             if user_input.lower() in ["q", "quit"]:
                 break
-            translated_line = translate_line(user_input)
+            translated_line = translate_line(user_input, nlp)
             print(f"Translated: {translated_line}")
 
-def translate_line(original_line):
-    # get parts of speech
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(original_line)
-    for token in doc:
-        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_)
+def translate_line(sentence, nlp):
+    word_list = preprocess(sentence, nlp)
+
+def preprocess(sentence, nlp):
+    doc = nlp(sentence)
+    # returns a nested list of form [[word, POS]...]
+    # lemmatize the verbs, removes determiners and punctuation
+    word_list = [(token.lemma_, token.pos_) for token in doc if token.pos_ != "PUNCT"]
+    # replace "who", pronouns and "do" with the things they refer too
+    for idx, (lemma, pos) in enumerate(word_list):
+        if lemma == "do":
+            word_list[idx] = get_closest_verb(word_list, idx)[0]
+        elif lemma == "who" or lemma== "too":
+            word_list[idx] = get_antecedent(word_list, idx)
+        elif lemma in ["he", "she", "her", "him", "herself", "himself"]:
+            word_list[idx] = get_first_noun(word_list, idx)
+    print(word_list)
+    return word_list
+
+def get_antecedent(word_list, idx):
+    for i in range(idx - 1, -1, -1):
+        lemma, pos = word_list[i]
+        if pos == "NOUN" or pos == "PROPN":
+            return (lemma, pos)
+    return None
+
+def get_closest_verb(word_list, idx):
+    # go backwards from the idx until we find a verb and return it
+    for i in range(idx-1, -1, -1):
+        if word_list[i] and word_list[i][1] == "VERB":
+            return (word_list[i], word_list[i+1])
+    return None
     
-    options = {"compact": True, "add_lemma": True}
-    displacy.serve(doc, auto_select_port=True)
-    displacy.serve(doc, style="dep", options = options)
+def get_first_noun(word_list, idx):
+    for i in range(0, len(word_list)):
+        lemma, pos = word_list[i]
+        if pos == "NOUN" or pos == "PROPN":
+            return (lemma, pos)
+    return None
 
 if __name__ == "__main__":
     main()
