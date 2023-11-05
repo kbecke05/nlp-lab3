6145842dddbe4b48d99487a7c4136d3777354f70
Kelly Becker <kbecke05@calpoly.edu>
2023-11-05 12:41:11 -0800

    started the conjunction section, still errors

--- chatFOL.py
+++ chatFOL.py
@@ -4,6 +4,7 @@ import nltk
 from spacy import displacy
 from nltk import load_parser
 from nltk.corpus import wordnet
+import spacy_transformers
 # from spacy.matcher import PhraseMatcher
 # from nltk.stem import WordNetLemmatizer
 
@@ -12,14 +13,16 @@ def main():
     parser.add_argument(dest = "file", nargs="?")
     args = parser.parse_args()
 
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load("en_core_web_trf")
     if args.file: # if text file is provided as an argument
         try:
             with open(args.file, "r") as file:
                 lines = file.readlines()
                 for line in lines:
                     original_line = line.strip()
-                    translated_line = translate_line(original_line, nlp)
+                    word_list = preprocess(original_line, nlp)
+                    #print(word_list)
+                    translated_line = translate_line(word_list, nlp)
                     print(f"Translated: {translated_line}\n")
         except FileNotFoundError:
             print(f"Error: File '{args.file}' not found.")
@@ -31,7 +34,9 @@ def main():
                 break
             if user_input.lower() in ["q", "quit"]:
                 break
-            translated_line = translate_line(user_input, nlp)
+            word_list = preprocess(user_input, nlp)
+            #print(word_list)
+            translated_line = translate_line(word_list, nlp)
             print(f"Translated: {translated_line}")
 
 def preprocess(sentence, nlp):
@@ -43,21 +48,23 @@ def preprocess(sentence, nlp):
     for idx, (lemma, pos) in enumerate(word_list):
         if lemma == "do":
             word_list[idx] = get_closest_verb(word_list, idx)[0]
+            if (idx+1 < len(word_list) and word_list[idx + 1][0] == "not"):
+                word_list[idx], word_list[idx+1] = word_list[idx + 1], word_list[idx]
         elif lemma == "who" or lemma== "too":
             word_list[idx] = get_antecedent(word_list, idx)
         elif lemma in ["he", "she", "her", "him", "herself", "himself"]:
             word_list[idx] = get_first_noun(word_list, idx)
     return word_list
 
-def translate_line(sentence, nlp):
-    word_list = preprocess(sentence, nlp)
-    print(word_list)
+def translate_line(word_list, nlp):
     count = 0
-    final_string = ""
+    print(word_list)
+    # final_string = ""
     # find number of verbs / adjs in sentence
     for (lemma, pos) in word_list:
         if pos == 'VERB' or pos == 'ADJ':
             count += 1
+    print("count = "+ str(count))
     if count == 0: # no clear descriptor -> look into auxillaries
         for i, (lemma, pos) in enumerate(word_list):
             if pos == 'AUX':
@@ -70,33 +77,42 @@ def translate_line(sentence, nlp):
                     for j in range(i, len(word_list)):
                         if word_list[j][1] == 'NOUN':
                             if N1:
-                                final_string += f"{word_list[j][0]}({N1}, {N2})"
+                                return f"{word_list[j][0]}({N1}, {N2})"
                             else:
-                                final_string += f"{word_list[j][0]}({N2})"
+                                return f"{word_list[j][0]}({N2})"
     elif count == 1:
         i, verb = [(i, v) for i, (v, pos) in enumerate(word_list) if pos == 'VERB' or pos == 'ADJ'][0]
         N1 = None
         N2 = None
-        if i > 0:
-            N2_pos = get_antecedent(word_list, i)
-            N2 = N2_pos[0]
-            if word_list[ word_list.index(N2_pos) -1][1] == 'CCONJ' or word_list[word_list.index(N2_pos)-2][1] == 'CCONJ': # ___ CCONJ (DET) N2 -> must be another noun 
-                N1 = get_antecedent(word_list, word_list.index(N2_pos) -1)[0]
+        if i > 0: # if the verb is not the very first word
+            N2_pos = get_antecedent(word_list, i) # get noun in front of verb
+            if N2_pos:
+                N2 = N2_pos[0]
+                if word_list[ word_list.index(N2_pos) -1][1] == 'CCONJ' or word_list[word_list.index(N2_pos)-2][1] == 'CCONJ': # ___ CCONJ (DET) N2 -> must be another noun 
+                    N1 = get_antecedent(word_list, word_list.index(N2_pos) -1)[0]
         if N1 == None:
             N1 = N2
             N2 = None
         if i < len(word_list) - 1:
-            N2 = get_subsequent(word_list, i)[0]
+            N2 = get_subsequent(word_list, i)
+            if N2:
+                N2 = N2[0]
         if N1 and N2:
-            final_string += f"{verb}({N1}, {N2})"
+            return f"{verb}({N1}, {N2})"
         elif N1:
-            final_string += f"{verb}({N1})"
+            return f"{verb}({N1})"
         elif N2:
-            final_string += f"{verb}({N2})"
+            return f"{verb}({N2})"
 
 
-    # elif count == 2: # look at conjunctions
-    return final_string
+    elif count == 2: # look at conjunctions
+        conj_map = {"but": "&", "and": "&", "or": "|", "if": "->"}
+        conj = get_conjunction(word_list)
+        word = conj[0] # the word
+        i = conj[2] #the index of the conj returned from get_conjunction()
+        return translate_line(word_list[:i], nlp) + " " + conj_map[word] + " " + translate_line(word_list[i+1:], nlp)
+
+    # return final_string
 
 def get_antecedent(word_list, idx):
     # go backwards from the idx until we find a noun and return it
@@ -126,6 +142,13 @@ def get_first_noun(word_list, idx):
         lemma, pos = word_list[i]
         if pos == "NOUN" or pos == "PROPN":
             return (lemma, pos)
+    return None
+
+def get_conjunction(word_list):
+    for i in range(0, len(word_list)):
+        lemma, pos = word_list[i]
+        if pos == "CCONJ":
+            return (lemma, pos, i)
     return None
 
 if __name__ == "__main__":
